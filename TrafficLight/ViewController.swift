220558e603a3bec72e29c92033bb969f72ecab17
Ibullayev Zhanibek <ibullayevzhanibek@gmail.com>
2024-06-09 01:52:16 +0500

    Create logic for traffic light

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -1,12 +1,58 @@
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightOff: CGFloat = 0.3
+    private let lightOn: CGFloat = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        startButton.layer.cornerRadius = 5
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
     }
+    
+    override func viewWillLayoutSubviews() {
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+    }
+    
+    @IBAction func startButtonDidTapped() {
+        if startButton.currentTitle == "Start" {
+            startButton.setTitle("Next", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenLightView.alpha = lightOff
+            redLightView.alpha = lightOn
+            currentLight = .yellow
+        case .yellow:
+            redLightView.alpha = lightOff
+            yellowLightView.alpha = lightOn
+            currentLight = .green
+        case .green:
+            yellowLightView.alpha = lightOff
+            greenLightView.alpha = lightOn
+            currentLight = .red
+        }
+    }
+}
 
-
+extension ViewController {
+    enum CurrentLight {
+        case red
+        case yellow
+        case green
+    }
 }
 
