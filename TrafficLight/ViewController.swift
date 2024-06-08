94f03b040013988a45d12a8c1d6f3d4827e681c1
Ibullayev Zhanibek <ibullayevzhanibek@gmail.com>
2024-06-09 00:43:37 +0500

    Prepare project. Delete comments

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  TrafficLight
-//
-//  Created by Zhanibek Ibullayev on 09.06.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
